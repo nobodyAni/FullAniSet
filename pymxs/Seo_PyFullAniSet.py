b80f86152c889870be6f05bb7cbb655de79c4863
살균표백 <NobodyInbody@outlook.com>
2020-03-19 22:19:29 +0900

    프로젝트셋팅

---
+++ pymxs/Seo_PyFullAniSet.py
@@ -0,0 +1,10 @@
+import MaxPlus
+import pymxs
+from PySide2 import QtWidgets, QtCore, QtGui
+RT = pymxs.runtime
+class AniSet():
+    def __init__(self, name, startFrame, endFrame):
+        self.name = name
+        self.start_frame = startFrame
+        self.end_frame = endFrame
+class FullAniSetView():
